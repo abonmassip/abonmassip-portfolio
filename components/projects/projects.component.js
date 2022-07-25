18d7f0ae032685925ca8aae0c562119ef242b9d8
Albert <abonmassip@gmail.com>
2022-07-25 08:44:04 -0700

    fix

--- components/projects/projects.component.js
+++ components/projects/projects.component.js
@@ -3,7 +3,7 @@ import { Project } from '../project/project.component'
 export function Projects({ projectsData, what }) {
   return (
     <div className="section" data-anchor={what}>
-      {projectsData.map(({ id, text, title, links }, i) => (
+      {projectsData.map(({ id, text, title, links }) => (
         <div className="slide" data-anchor={id} key={id}>
           <Project image={`/img/${id}.jpg`} title={title} text={text} links={links} />
         </div>
